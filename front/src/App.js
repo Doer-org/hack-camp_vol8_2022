0ce601d28e63ec0f4440fdbb1619b7d441e9fb39
39TO <ohisama.0610@icloud.com>
2022-11-20 11:46:56 +0900

    tailwind いけてるか

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-red">Hello world!</h1>
+        <h1 className="text-red-200">Hello world!</h1>
       </header>
     </div>
   )
