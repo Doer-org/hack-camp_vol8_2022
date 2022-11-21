1e9831ecb8af99bca9eb70ed7843e9cf2621583b
39TO <ohisama.0610@icloud.com>
2022-11-21 11:15:50 +0900

    大まかなUI　作成

---
+++ front/src/components/layout/AppContainer.jsx
@@ -0,0 +1,7 @@
+export const AppContainer = ({ children }) => {
+  return (
+    <main className="mx-5 md:w-full md:mx-auto p-6 max-w-2xl grow drop-shadow-xl border-2 rounded-md my-5">
+      {children}
+    </main>
+  );
+};
--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { PeaceIcon } from '../icon/PeaceIcon';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="py-6 lg:py-4 text-stone-800 flex-none text-black-800 text-3xl md:text-5xl font-bold gap-2.5">
+      <div className="flex items-center justify-center">
+        <PeaceIcon />
+        Warikan Generator
+      </div>
+    </div>
+  );
 };
