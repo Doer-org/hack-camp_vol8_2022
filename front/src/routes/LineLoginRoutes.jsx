5dd4cfc1b5485433a3d6c6758959b4b4caa92970
39TO <ohisama.0610@icloud.com>
2022-11-24 14:21:55 +0900

    リファクタリング

--- front/src/routes/LineLoginRoutes.jsx
+++ front/src/routes/LineLoginRoutes.jsx
@@ -1,17 +1,11 @@
-import { generateRandomString } from '../hooks/generateRandomString';
 import { isAuthenticatedState } from '../hooks/sessionStore';
-import axios from 'axios';
+import {
+  HandleProviderCallback,
+  RedirectToProvider
+} from '../hooks/useLineLogin';
 import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate, useSearchParams } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
-
-const state = generateRandomString();
-const client_id = '1657672330';
-const redirect_uri = encodeURI(
-  'https://warikan-generator.vercel.app/line/callback'
-);
-const client_secret = 'bafde86582cd2ba675804f11d3092893';
-const url = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=profile`;
 
 export const LineLoginRoutes = () => {
   const [session] = useRecoilState(isAuthenticatedState);
@@ -21,53 +15,6 @@ export const LineLoginRoutes = () => {
       navigate('/home');
     }
   }, [session]);
-  console.log('url', url);
-  console.log('state', state);
-
-  function RedirectToProvider() {
-    // 👇️ redirect to external URL
-    console.log(url);
-    window.location.replace(url);
-
-    return null;
-  }
-
-  function HandleProviderCallback() {
-    const [queryParameters] = useSearchParams();
-    const returnCode = queryParameters.get('code');
-    const returnState = queryParameters.get('state');
-
-    var params = new URLSearchParams();
-    params.append('grant_type', 'authorization_code');
-    params.append('code', returnCode);
-    params.append('redirect_uri', redirect_uri);
-    params.append('client_id', client_id);
-    params.append('client_secret', client_secret);
-
-    console.log('lastState', state);
-    console.log('returnState', returnState);
-
-    if (returnState === state) {
-      axios
-        .post('https://api.line.me/oauth2/v2.1/token', params)
-        .then((res) => {
-          const accessToken = res.data.access_token;
-
-          axios
-            .get('https://api.line.me/v2/profile', {
-              headers: {
-                Authorization: `Bearer ${accessToken}`
-              }
-            })
-            .then((res) => {
-              console.log(res.data);
-            });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }
 
   return (
     <Routes>
