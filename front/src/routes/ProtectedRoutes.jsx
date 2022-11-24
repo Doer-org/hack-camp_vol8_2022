6c5d65b67e1ae19df99f5bf3c6cdd021c41e741d
39TO <ohisama.0610@icloud.com>
2022-11-24 15:31:55 +0900

    fix

--- front/src/routes/ProtectedRoutes.jsx
+++ front/src/routes/ProtectedRoutes.jsx
@@ -1,12 +1,21 @@
+import { isAuthenticatedState } from '../hooks/sessionStore';
 import { CompleteCreateEvent } from '../pages/CompleteCreateEvent';
 import { CreateEvent } from '../pages/CreateEvent';
 import { Event } from '../pages/Event';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
 export const ProtectedRoutes = () => {
+  const [session] = useRecoilState(isAuthenticatedState);
+  useEffect(() => {
+    if (session) {
+      Navigate('/home');
+    }
+  }, [session]);
+
   return (
     <Routes>
       <Route path="home" element={<Home />} />
