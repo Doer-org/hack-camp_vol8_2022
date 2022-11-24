bbbeb937a4d1aea175acbd26dada1a466d748b66
39TO <ohisama.0610@icloud.com>
2022-11-24 13:24:01 +0900

    fix

--- front/src/routes/LineLoginRoutes.jsx
+++ front/src/routes/LineLoginRoutes.jsx
@@ -26,7 +26,8 @@ export const LineLoginRoutes = () => {
 
   function RedirectToProvider() {
     // 👇️ redirect to external URL
-    window.location.replace(url);
+    // window.location.replace(url);
+    console.log(url);
 
     return null;
   }
