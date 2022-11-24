73d4514b510f8b158e0f5fa9bbb98a509ac99b24
39TO <ohisama.0610@icloud.com>
2022-11-24 17:49:15 +0900

    fix UI

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -5,11 +5,13 @@ import { useRecoilState } from 'recoil';
 export const Header = () => {
   const [session] = useRecoilState(isAuthenticatedState);
   return (
-    <div className="py-4 lg:py-4 mx-3 text-stone-800 flex-none text-black-800 text-3xl md:text-5xl font-bold gap-2.5">
-      <div className="flex items-center justify-center ml-5">
-        <PeaceIcon />
-        Warikan Generator
-        <div className="shadow-lg rounded-2xl bg-white p-2 mr-5 ml-auto">
+    <div className="max-w-2xl md:mx-auto pt-5 pb-3">
+      <div className="flex items-center justify-center text-stone-800 text-2xl md:text-3xl font-semibold">
+        <div className="ml-3 mr-auto flex items-center">
+          <PeaceIcon />
+          Warikan Generator
+        </div>
+        <div className="shadow-lg rounded-2xl bg-white p-2 mr-2 ml-auto md:ml-20">
           <div className="flex items-center">
             <img
               alt="profile"
