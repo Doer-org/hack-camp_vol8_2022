4c04d5579eed874e3ce19303d4fe8e88184f3d91
39TO <ohisama.0610@icloud.com>
2022-11-24 13:31:17 +0900

    fix

--- front/src/hooks/generateRandomString.js
+++ front/src/hooks/generateRandomString.js
@@ -1,9 +1,7 @@
-import { useCallback } from 'react';
-
-export const generateRandomString = useCallback(() => {
+export const generateRandomString = () => {
   var S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   var N = 16;
   return Array.from(Array(N))
     .map(() => S[Math.floor(Math.random() * S.length)])
     .join('');
-}, []);
+};
