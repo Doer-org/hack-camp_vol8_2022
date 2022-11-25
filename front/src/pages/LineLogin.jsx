cfbc06905a8265179dc7fc2c9df23525bbedb967
39TO <ohisama.0610@icloud.com>
2022-11-25 20:59:09 +0900

    fix

--- front/src/pages/LineLogin.jsx
+++ front/src/pages/LineLogin.jsx
@@ -1,9 +1,10 @@
 import picture from '../Images/line_button.png';
 import { useLocation } from 'react-router-dom';
 
-export const LineLogin = ({ path }) => {
-  // eslint-disable-next-line no-unused-vars
-  path = useLocation().pathname;
+export const LineLogin = ({ setState }) => {
+  const path = useLocation().pathname;
+  setState(path);
+
   return (
     <div className="mt-10 text-center">
       <h1 className="text-gray-800 text-2xl md:text-3xl font-bold text-center mb-10">
--- front/src/routes/LineLoginRoutes.jsx
+++ front/src/routes/LineLoginRoutes.jsx
@@ -5,14 +5,10 @@ import {
 import { LineLogin } from '../pages/LineLogin';
 import React from 'react';
 import { useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 export const LineLoginRoutes = () => {
   const [state, setState] = useState('/');
-  const pathname = useLocation().pathname;
-  if (pathname !== '/login' || pathname !== '/login/callback') {
-    setState(pathname);
-  }
   return (
     <Routes>
       <Route path="/login" element={<RedirectToProvider />} />
@@ -20,7 +16,7 @@ export const LineLoginRoutes = () => {
         path="/line/callback"
         element={<HandleProviderCallback path={state} />}
       />
-      <Route path="*" element={<LineLogin />} />
+      <Route path="*" element={<LineLogin setState={setState} />} />
     </Routes>
   );
 };
