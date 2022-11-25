c19c026ced4026bc50ae3d2f0271af57c8b8b37a
39TO <ohisama.0610@icloud.com>
2022-11-25 18:39:40 +0900

    fix

--- front/src/routes/LineLoginRoutes.jsx
+++ front/src/routes/LineLoginRoutes.jsx
@@ -15,7 +15,7 @@ export const LineLoginRoutes = () => {
       <Route path="/login" element={<RedirectToProvider />} />
       <Route
         path="/line/callback"
-        element={<HandleProviderCallback location={location.pathname} />}
+        element={<HandleProviderCallback pathname={location.pathname} />}
       />
       <Route path="*" element={<LineLogin />} />
     </Routes>
