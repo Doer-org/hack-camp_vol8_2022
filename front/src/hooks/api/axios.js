ba2432498c79adc71de30317e0be0f4ff3c67ddb
39TO <ohisama.0610@icloud.com>
2022-11-27 00:24:10 +0900

    withCredentials

--- front/src/hooks/api/axios.js
+++ front/src/hooks/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const $axios = axios.create({
-  baseURL: 'https://warikan-sb4awdmn4q-an.a.run.app'
-  // headers: {
-  //   'Access-Control-Allow-Origin': '*'
-  // }
+  baseURL: 'https://warikan-sb4awdmn4q-an.a.run.app',
+  headers: {
+    withCredentials: true
+  }
 });
