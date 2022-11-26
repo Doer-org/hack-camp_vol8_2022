6b109e8f85563d72771df8dce44ea9adea36a797
39TO <ohisama.0610@icloud.com>
2022-11-27 02:23:38 +0900

    fix

--- front/src/hooks/api/axios.js
+++ front/src/hooks/api/axios.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 export const $axios = axios.create({
   baseURL: 'https://warikan-sb4awdmn4q-an.a.run.app',
-  withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers':
-      'Origin, X-Requested-With, Content-Type, Accept'
-  }
+  withCredentials: true
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Headers': 'Content-Type'
+  // }
 });
