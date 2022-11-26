07de7183567ffc6e658b321f6a95b4c9eca51146
39TO <ohisama.0610@icloud.com>
2022-11-27 06:47:57 +0900

    fix

--- front/src/pages/CompleteCreateEvent.jsx
+++ front/src/pages/CompleteCreateEvent.jsx
@@ -43,7 +43,7 @@ export const CompleteCreateEvent = () => {
             onClick={copy}
           />
         </div>
-        <div className="mb-20 mx-auto">
+        <div className="mb-20 mx-auto w-1/2">
           <QRCode value={url} />
         </div>
         <hr />
