31b883fa7e31124491022c882cff7578b3293ab1
clcl777 <77223796+clcl777@users.noreply.github.com>
2022-11-26 22:42:33 +0900

    CROS設定

--- server/src/presentation/router/scheduler.py
+++ server/src/presentation/router/scheduler.py
@@ -1,24 +1,23 @@
 from flask import Blueprint
 
-# from config.db.session import session
-# from presentation.controller.schedluer import SchedluerController
-# from repository.schedluer import SchedluerRepository
-# from service.schedluer import SchedluerService
+from config.db.session import session
+from presentation.controller.scheduler import SchedulerController
+from repository.scheduler import SchedulerRepository
+from service.scheduler import SchedulerService
 
-import notification.scheduler
+# import notification.scheduler
 
 # api
 api = Blueprint("scheduler", __name__)
 
 # private variable
-# __r = SchedluerRepository(session)
-# __s = SchedluerService(__r)
-# __c = SchedluerController(__s)
+__r = SchedulerRepository(session)
+__s = SchedulerService(__r)
+__c = SchedulerController(__s)
 
 
 @api.route("/scheduler", methods=["GET"])
 def get():
-
-    # return __c.get()
-    notification.scheduler.scheduler()
+    return __c.get()
+    # notification.scheduler.scheduler()
     return "scheduler完了"
