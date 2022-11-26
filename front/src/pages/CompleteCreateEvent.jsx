4df486b16f8e47df96f5fdcdb2144be683903f34
39TO <ohisama.0610@icloud.com>
2022-11-27 07:02:10 +0900

    元に戻す

--- front/src/pages/CompleteCreateEvent.jsx
+++ front/src/pages/CompleteCreateEvent.jsx
@@ -43,7 +43,7 @@ export const CompleteCreateEvent = () => {
             onClick={copy}
           />
         </div>
-        <div className="mb-20 flex items-center justify-center">
+        <div className="mb-20 flex justify-center">
           <QRCode value={url} />
         </div>
         <hr />
