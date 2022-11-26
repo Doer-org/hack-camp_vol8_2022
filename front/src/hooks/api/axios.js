e257e8a8d7b7728d05f41558d900c61a25db9d65
39TO <ohisama.0610@icloud.com>
2022-11-27 00:37:27 +0900

    頼む

--- front/src/hooks/api/axios.js
+++ front/src/hooks/api/axios.js
@@ -2,7 +2,5 @@ import axios from 'axios';
 
 export const $axios = axios.create({
   baseURL: 'https://warikan-sb4awdmn4q-an.a.run.app',
-  headers: {
-    withCredentials: true
-  }
+  withCredentials: true
 });
