cb7011d77ff7c5ff63d87a6dd0f33b149d26954b
clcl777 <77223796+clcl777@users.noreply.github.com>
2022-11-26 14:46:02 +0900

    修正

--- server/src/presentation/router/router.py
+++ server/src/presentation/router/router.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from presentation.router import health, user, event, health_line
 # from presentation.router import health, user, event, health_line, scheduler
-from presentation.router import health, user, event, health_line, scheduler
 
 app = Flask(__name__)
 
@@ -8,5 +8,4 @@ app.register_blueprint(health.api)
 app.register_blueprint(user.api)
 app.register_blueprint(event.api)
 app.register_blueprint(health_line.api)
-app.register_blueprint(scheduler.api)
 # app.register_blueprint(scheduler.api)
