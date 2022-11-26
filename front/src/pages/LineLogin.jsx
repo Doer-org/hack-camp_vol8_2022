5b73d6327c6ccd508965cb7f1e35da3a99d7faf1
39TO <ohisama.0610@icloud.com>
2022-11-26 19:01:56 +0900

    fix

--- front/src/pages/LineLogin.jsx
+++ front/src/pages/LineLogin.jsx
@@ -1,11 +1,14 @@
 import picture from '../Images/line_button.png';
 import { redirect_path } from '../hooks/sessionStore';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
 export const LineLogin = () => {
   const [, setPath] = useRecoilState(redirect_path);
   const location = useLocation();
+  const route = useRoutes();
+  console.log('location', location);
+  console.log('route', route);
   setPath(location.pathname);
   return (
     <div className="mt-10 text-center">
@@ -16,7 +19,6 @@ export const LineLogin = () => {
       <a href="/login" className="w-32 inline-block">
         <img src={picture} alt="LINEログイン" className="w-32 mx-auto" />
       </a>
-      <div className="text-red-500">{location.state.message}</div>
     </div>
   );
 };
