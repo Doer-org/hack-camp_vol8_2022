e96e431f582e7a5ba7bdd4c16faf51ab8d18e3df
kimuran25 <110321511+kimuran25@users.noreply.github.com>
2022-11-27 03:25:06 +0900

    fix

--- server/src/presentation/router/router.py
+++ server/src/presentation/router/router.py
@@ -1,6 +1,13 @@
 from flask import Flask
-from presentation.router import event, health, health_line, status, user, scheduler
 from flask_cors import CORS
+from presentation.router import (
+    event,
+    health,
+    health_line,
+    scheduler,
+    status,
+    user,
+)
 
 app = Flask(__name__)
 
@@ -10,17 +17,18 @@ app.register_blueprint(event.api)
 app.register_blueprint(status.api)
 app.register_blueprint(health_line.api)
 app.register_blueprint(scheduler.api)
-# CORS(
-#     app,
-#     supports_credentials=True
-#     origins=["https://warikan-generator.vercel.app"]
-# )
+
+CORS(
+    app,
+    supports_credentials=True,
+    origins=["https://warikan-generator.vercel.app"],
+)
 
 
-@app.after_request
-def after_request(res):
-    res.headers.add('Access-Control-Allow-Origin', 'https://warikan-generator.vercel.app')
-    res.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    res.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.headers.add('Access-Control-Allow-Credentials', 'true')
-    return res
+# @app.after_request
+# def after_request(res):
+#     res.headers.add('Access-Control-Allow-Origin', 'https://warikan-generator.vercel.app')
+#     res.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+#     res.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#     res.headers.add('Access-Control-Allow-Credentials', 'true')
+#     return res
