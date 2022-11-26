90939502d0f9e233d4c46b6201013cbb06ffa868
39TO <ohisama.0610@icloud.com>
2022-11-26 23:40:59 +0900

    /event

--- front/src/hooks/api/useCreateEvent.js
+++ front/src/hooks/api/useCreateEvent.js
@@ -1,20 +1,18 @@
 import { $axios } from './axios';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
-axios.defaults.baseURL = 'https://warikan-sb4awdmn4q-an.a.run.app';
 
 export const useCreateEvent = () => {
   const createEvent = async (data) => {
     const navigate = useNavigate();
     await $axios
-      .post('event', data)
+      .post('/event', data)
       .then((res) => {
         navigate('/new/complete', {
           state: { id: res.data.id }
         });
       })
       .catch((error) => {
+        console.log('イベント作成失敗');
         throw error;
       });
   };
