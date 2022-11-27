7a01b6b0ec8bbb97a1d8da50dc41d810ba74c048
kimuran25 <110321511+kimuran25@users.noreply.github.com>
2022-11-27 14:44:32 +0900

    Update statuspostAPI

--- server/src/repository/status.py
+++ server/src/repository/status.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 
 from model.status import Status
+from repository.event_dto import EventDto
 from repository.status_dto import StatusDto
+from repository.user_dto import UserDto
 from sqlalchemy.orm.session import Session
 from utils.error.error import Error
 
@@ -12,6 +14,32 @@ class StatusRepository:
 
     # status情報を保存する
     def create(self, s: Status) -> Tuple[Status, Error]:
+        row_user = (
+            self.__session.query(UserDto)
+            .filter(s.user_id == UserDto.id)
+            .first()
+        )
+        if row_user == None:
+            return None, Error(msg="user_id is None")
+
+        row_event = (
+            self.__session.query(EventDto)
+            .filter(s.event_id == EventDto.id)
+            .first()
+        )
+        if row_event == None:
+            return None, Error(msg="event_id is None")
+
+        row = (
+            self.__session.query(StatusDto)
+            .filter(s.user_id == StatusDto.user_id)
+            .filter(s.event_id == StatusDto.event_id)
+            .first()
+        )
+
+        if row != None:
+            return None, Error(msg="status create error")
+
         dto = StatusDto()
         dto.user_id = s.user_id
         dto.event_id = s.event_id
