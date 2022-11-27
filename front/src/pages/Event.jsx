663690c4ef7f7d3d7bcca0a8b2da67ff06cf0d6e
39TO <ohisama.0610@icloud.com>
2022-11-27 11:28:44 +0900

    push

--- front/src/pages/Event.jsx
+++ front/src/pages/Event.jsx
@@ -2,21 +2,18 @@ import { EventOfAdmin } from '../components/modules/EventOfAdmin';
 import { EventOfMember } from '../components/modules/EventOfMember';
 import { $axios } from 'hooks/api/axios';
 import { isAuthenticatedState } from 'hooks/sessionStore';
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
 export const Event = () => {
   const { id } = useParams();
   const [session] = useRecoilState(isAuthenticatedState);
-  const [state, setEvent] = useState(null);
 
   const getEvent = async () => {
     const event = await $axios
       .get(`/event/${id}`)
       .then((res) => {
         console.log(res);
-        setEvent(res.data);
       })
       .catch((error) => {
         throw error;
@@ -27,12 +24,6 @@ export const Event = () => {
 
   const event = getEvent();
   console.log('event', event);
-  console.log('state', state);
-
-  // const { eventRes } = useEvent(id);
-  // useEffect(() => {
-  //   console.log('eventRes', eventRes);
-  // }, []);
 
   // イベントのadmin_idがログインユーザーのidと一致するかどうかで表示を分ける
   if (event.admin_id === session.id) {
