f63c4c44ad5987e60f706c1b54a95dadcaf97212
39TO <ohisama.0610@icloud.com>
2022-11-27 14:22:48 +0900

    fix

--- front/src/hooks/api/useEvent.js
+++ front/src/hooks/api/useEvent.js
@@ -5,25 +5,10 @@ import { useRecoilState } from 'recoil';
 export const useEvent = () => {
   const [session] = useRecoilState(isAuthenticatedState);
 
-  const participateEvent = async (event_id) => {
-    const event = await $axios
-      .post(`/status`, {
-        id: session.id,
-        event_id: event_id
-      })
-      .then((res) => {
-        return res.data;
-      })
-      .catch((error) => {
-        throw error;
-      });
-    return event;
-  };
-
   const completePayment = async (user_id, event_id) => {
     const event = await $axios
       .post(`/status`, {
-        id: user_id,
+        user_id: user_id,
         event_id: event_id
       })
       .then((res) => {
