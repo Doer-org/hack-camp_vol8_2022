0aefe63c434b6e9629995e52983e70ee2b0de4e7
39TO <ohisama.0610@icloud.com>
2022-11-27 11:45:22 +0900

    これで

--- front/src/App.js
+++ front/src/App.js
@@ -3,24 +3,20 @@ import { AppContainer } from './components/layout/AppContainer';
 import { Footer } from './components/layout/Footer';
 import { Header } from './components/layout/Header';
 import { AppRoutes } from './routes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-const queryClient = new QueryClient();
 function App() {
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <AppContainer>
-              <AppRoutes />
-            </AppContainer>
-            <Footer />
-          </div>
-        </RecoilRoot>
-      </QueryClientProvider>
+      <RecoilRoot>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <AppContainer>
+            <AppRoutes />
+          </AppContainer>
+          <Footer />
+        </div>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
