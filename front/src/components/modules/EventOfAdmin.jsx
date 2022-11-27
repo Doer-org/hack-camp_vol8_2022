26bd93b0f5adb9f33ad4abdc015d6dc96ac48e8a
39TO <ohisama.0610@icloud.com>
2022-11-27 14:20:22 +0900

    hey

--- front/src/components/modules/EventOfAdmin.jsx
+++ front/src/components/modules/EventOfAdmin.jsx
@@ -41,7 +41,35 @@ export const EventOfAdmin = ({ event }) => {
       <div className="container flex flex-col mx-auto w-72 items-center justify-center bg-white rounded-lg shadow">
         <ul className="flex flex-col divide divide-y">
           {event.participants.map((participant, index) => {
-            return <UserRow participant={participant} key={index} />;
+            return (
+              <li className="flex flex-row" key={index}>
+                <div className="select-none cursor-pointer flex flex-1 items-center p-4">
+                  <div className="flex flex-col w-10 h-10 justify-center items-center mr-4">
+                    <img
+                      alt="profile"
+                      src={participant.user.picture_url}
+                      className="mx-auto object-cover rounded-full h-10 w-10 "
+                    />
+                  </div>
+                  <div className="flex-1 pl-1 mr-16">
+                    <div className="font-medium">
+                      {participant.user.display_name}
+                    </div>
+                  </div>
+                  <BaseButton
+                    label="完了"
+                    color="bg-purple-600 hover:bg-purple-700 text-white"
+                    type="submit"
+                    onClick={() => {
+                      completePayment(
+                        participant.user.id,
+                        participant.status.event_id
+                      );
+                    }}
+                  />
+                </div>
+              </li>
+            );
           })}
         </ul>
       </div>
